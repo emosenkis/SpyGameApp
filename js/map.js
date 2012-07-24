e40c8d091c08bcbaf826ba39c01c00a373c9bf03
Eitan Mosenkis <eitan@mosenkis.net>
2012-07-24 16:17:45 -0400

    Improve map display

--- js/map.js
+++ js/map.js
@@ -11,6 +11,10 @@ function initialize() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		panControl: false,
 		zoomControl: false,
+		disableDoubleClickZoom: true,
+		draggable: false,
+		keyboardShortcuts: false,
+		scrollwheel: false
 	};
 	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 }
@@ -18,46 +22,62 @@ function initialize() {
 function update_positions(data) {
 	//if marker exists, update its location, else create it
 	var detectives = data.detectives;
+	var uid;
 	for (var i = 0; i < detectives.length; i++) {
 		d = detectives[i];
-		if (markerList.hasOwnProperty(d.user_id)) {
-			moveMarker(markerList[d.user_id], d.latitude, d.longitude);
+		uid=d.user_id == data.user_id ? 'me' : d.user_id;
+		if (markerList.hasOwnProperty(uid)) {
+			moveMarker(markerList[uid], d.latitude, d.longitude);
 		} else {
-			addMarker(d.latitude, d.longitude, d.user_id, d.name);
+			addMarker(d.latitude, d.longitude, uid, d.name, 'detective');
 		}
 	}
 	var spy = data.spy;
-	if (markerList.hasOwnProperty(spy.user_id)) {
-		moveMarker(markerList[spy.user_id], spy.latitude, spy.longitude);
+	uid=spy.user_id == data.user_id ? 'me' : spy.user_id;
+	if (markerList.hasOwnProperty(uid)) {
+		moveMarker(markerList[uid], spy.latitude, spy.longitude);
 	} else {
-		addMarker(spy.latitude, spy.longitude, spy.user_id, spy.name, true);
+		addMarker(spy.latitude, spy.longitude, uid, spy.name, 'spy');
 	}
+
+	zoomToFit();
 }
 
-function moveMarker(marker, lat, long) {
-	marker.setPosition(new google.maps.LatLng(lat, long));
+function zoomToFit() {
+	var bounds=new google.maps.LatLngBounds();
+	for (id in markerList) {
+		bounds.extend(markerList[id].position);
+	}
+
+	map.fitBounds(bounds);
 }
 
-function addMarker(lat, long, id, name, is_spy) {
-	var pinColor = is_spy ? "FF0000" : "00FFFF";
-	var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-		new google.maps.Size(21, 34),
-		new google.maps.Point(0, 0),
-		new google.maps.Point(10, 34));
-	var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-		new google.maps.Size(40, 37),
-		new google.maps.Point(0, 0),
-		new google.maps.Point(12, 35));
+function moveMarker(marker, lat, lng) {
+	marker.setPosition(new google.maps.LatLng(lat, lng));
+}
+
+function addMarker(lat, lng, id, name, role) {
+	var pinImage;
+	if (role == 'detective') {
+		pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|00FFFF",
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0, 0),
+			new google.maps.Point(10, 34));
+	} else {
+		pinImage = new google.maps.MarkerImage(id == 'me' ? 'images/spy-me-small.png' : 'images/spy-small.png',
+			new google.maps.Size(18, 28),
+			new google.maps.Point(0, 0),
+			new google.maps.Point(9, 25));
+	}
 	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(lat, long),
+		position: new google.maps.LatLng(lat, lng),
 		map: map,
 		title: name,
-		icon: pinImage,
-		shadow: pinShadow
+		icon: pinImage
 	});
 	markerList[id] = marker;
 	//DROP MARKER INFOWINDOW
-	var latlng = new google.maps.LatLng(lat, long);
+	var latlng = new google.maps.LatLng(lat, lng);
 	google.maps.event.addListener(marker, 'click', function () {
 		infowindow.open(map, this);
 		marker = this;
@@ -68,6 +88,7 @@ function addMarker(lat, long, id, name, is_spy) {
 	//CLOSING MARKER INFOWINDOW
 	google.maps.event.addListener(infowindow, 'closeclick', function () {
 		infowindow.close(map, this); //removes the marker
+		zoomToFit();
 	});
 }
 
@@ -82,10 +103,15 @@ function geolocationSuccess(position) {
 	}
 	console.log('Got position');
 	console.log(coords);
-	if (coords.accuracy < 100) {
-		document.last_loc=coords;
-		map.panTo(new google.maps.LatLng(coords.latitude, coords.longitude));
+	if (coords.accuracy >= 100) {
+		return;
 	}
+	document.last_loc=coords;
+	if (markerList.me) {
+		moveMarker(markerList.me, coords.latitude, coords.longitude);
+		zoomToFit();
+	}
+//	map.panTo(new google.maps.LatLng(coords.latitude, coords.longitude));
 }
 
 function pushPosition(id) {
